87f217793ae76839e75f9a289cacbb20b2a3a17c
Zachary Miller <zachary.m.miller@gmail.com>
2014-11-07 10:33:21 -0800

    RENAME: Generic configuration files.

--- util/supervisor.sh
+++ util/supervisor.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Site and environment set by supervisor.
+SITE=$1
+ENV=$2
+
 export LD_LIBRARY_PATH=/opt/instantclient_11_2/:/usr/local/lib/
 set -e
-SITE=moto
+
 BASEDIR=/opt/web/$SITE
 SITEDIR=$BASEDIR/site
 LOGFILE=$BASEDIR/log/gunicorn-$SITE.log
@@ -15,7 +20,7 @@ ADDRESS=0.0.0.0:9002
 cd $SITEDIR
 source $BASEDIR/bin/activate
 test -d $(dirname $LOGFILE) || mkdir -p $LOGDIR
-nuhup python manage.py collectstatic --noinput &
-exec gunicorn conf.moto_wsgi:application -w $NUM_WORKERS --bind=$ADDRESS \
-  --user=$USER --group=$GROUP --timeout=$TIMEOUT \
+nohup python manage.py collectstatic --noinput &
+exec gunicorn "conf.wsgi:application" -w $NUM_WORKERS --bind=$ADDRESS \
+  --user=$USER --group=$GROUP --timeout=$TIMEOUT --settings=conf.settings_$ENV \
   --log-file=$LOGFILE 2>>$LOGFILE --log-level=debug
