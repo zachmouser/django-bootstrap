b70511b4416e50d7068d937815270bd43eb6216b
Zachary Miller <zachary.m.miller@gmail.com>
2014-11-07 14:36:07 -0800

    FIX: Use django-admin startproject to set configuration files.

--- util/fabfile.py
+++ util/fabfile.py
@@ -1,35 +1,48 @@
+#!/usr/bin/python
+
+from contextlib import contextmanager
+
 from fabric.api import *
 from fabric.contrib import files
 
+_env = None
+
 def install_virtualenv():
 
-    sudo('apt-get install virtualenv')
+    sudo('apt-get install python-pip')
+    sudo('pip install virtualenv')
+
+@contextmanager
+def source_virtualenv():
+    with prefix('source {env}/bin/activate'.format(env=_env)):
+        yield
 
 def start_virtualenv():
 
     env_name = prompt('Virtual environment name: ')
     location = prompt('Environment location: ', default='/opt/env')
-    env = '{location}/{env}'.format(location=location, env=env_name)
+    _env = '{location}/{env}'.format(location=location, env=env_name)
 
     sudo('mkdir -p {env}'.format(env=env))
-    sudo('virtualenv {env} --no-site-packages'.format(env=env))
+    sudo('virtualenv {env} --no-site-packages'.format(env=_env))
 
-    run('cd {env}'.format(env=env))
-    run('source ./bin/activate')
+    run('cd {env}'.format(env=_env))
+
+    with source_virtualenv():
+        install_django()
 
 def install_django():
 
     version = prompt('Install Django version: ', default='1.7.1')
-    run('pip install Django=={version}'.format(version=version)
+    sudo('pip install Django=={version}'.format(version=version))
 
 def install_django_project():
-
-    install_django()
 
     project_name = prompt('Project name: ')
     git_repo = prompt('Repository location: ')
 
-    sudo('django-admin startproject --extension=py,conf --template={repo} {project}'.format(repo=git_repo, project=project_name))
+    with source_environment(env=_env):
+        sudo('django-admin startproject --extension=py,conf --template={repo} {project}'.format(repo=git_repo, project=project_name))
 
 if __name__ == '__main__':
 
