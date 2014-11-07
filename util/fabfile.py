d1db0d51008f764618765d6dde94365ffd33c20c
Zachary Miller <zachary.m.miller@gmail.com>
2014-11-07 14:00:08 -0800

    UPDATE: Fabric for testing.

--- util/fabfile.py
+++ util/fabfile.py
@@ -1,3 +1,38 @@
-import argparse
+from fabric.api import *
+from fabric.contrib import files
 
-im
+def install_virtualenv():
+
+    sudo('apt-get install virtualenv')
+
+def start_virtualenv():
+
+    env_name = prompt('Virtual environment name: ')
+    location = prompt('Environment location: ', default='/opt/env')
+    env = '{location}/{env}'.format(location=location, env=env_name)
+
+    sudo('mkdir -p {env}'.format(env=env))
+    sudo('virtualenv {env} --no-site-packages'.format(env=env))
+
+    run('cd {env}'.format(env=env))
+    run('source ./bin/activate')
+
+def install_django():
+
+    version = prompt('Install Django version: ', default='1.7.1')
+    run('pip install Django=={version}'.format(version=version)
+
+def install_django_project():
+
+    install_django()
+
+    project_name = prompt('Project name: ')
+    git_repo = prompt('Repository location: ')
+
+    sudo('django-admin startproject --extension=py,conf --template={repo} {project}'.format(repo=git_repo, project=project_name))
+
+if __name__ == '__main__':
+
+    install_virtualenv()
+    start_virtualenv()
+    install_django_project()
