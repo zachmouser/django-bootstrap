b0508b3e4fa09b94e25023bf873c1ebd752f7547
Zachary Miller <zachary.m.miller@gmail.com>
2014-11-14 12:19:47 -0800

    UPDATE: fabfile changes.

--- util/fabfile.py
+++ util/fabfile.py
@@ -3,7 +3,7 @@
 from contextlib import contextmanager
 
 from fabric.api import *
-from fabric.colors import green
+from fabric.colors import green, yellow
 from fabric.contrib import files
 
 class setup(object):
@@ -11,6 +11,9 @@ class setup(object):
     def __init__(self):
         self._path = None
         self._project_name = prompt(green('Project name: '))
+
+        self.nginx_conf = None
+        self.supervisor_conf = None
 
     def install_virtualenv(self):
 
@@ -28,6 +31,12 @@ class setup(object):
         self._path = '{location}/{env}'.format(location=location, env=self._project_name)
 
         sudo('mkdir -p {env}'.format(env=self._path))
+        if files.exists(self._path):
+            overwrite = prompt(yellow('A prior installation exists at [{path}]. Remove it (y / n)? '.format(path=self._path)), default='n', validate='^(y|n)$')
+            if overwrite == 'y': self.teardown()
+        else:
+            raise self.environmentExistsException('A project already exists at {p}'.format(p=self._path))
+
         sudo('virtualenv {env} --no-site-packages'.format(env=self._path))
 
         run('cd {env}'.format(env=self._path))
@@ -69,6 +78,7 @@ class setup(object):
 
             conf = '/etc/nginx/sites-enabled/nginx-{project}.conf'.format(project=self._project_name)
             sudo('mv {nginx} {conf}'.format(nginx=nginx, conf=conf))
+            self.nginx_conf = conf
 
             sudo('service nginx restart')
 
@@ -89,16 +99,32 @@ class setup(object):
 
             conf = '/etc/supervisor/conf.d/supervisor-{project}.conf'.format(project=self._project_name)
             sudo('mv {supervisor} {conf}'.format(supervisor=supervisor, conf=conf))
+            self.supervisor_conf = conf
 
             sudo('supervisorctl reload')
             sudo('supervisorctl start {project}'.format(project=self._project_name))
 
             self.configure_nginx(port)
 
+    def teardown(self):
+
+        if self.nginx_conf: sudo('rm {nc}'.format(nc=self.nginx_conf))
+        if self.supervisor_conf: sudo('rm {sc}'.format(sc=self.supervisor_conf))
+        if files.exists(self._path): sudo('rm -rf {p}'.format(p=self._path))
+
+    class environmentExistsException(Exception):
+
+        def __init__(self, message): self.message = message
+        def __str__(self): return self.message
+
 if __name__ == '__main__':
 
     s = setup()
-    s.install_virtualenv()
-    s.start_virtualenv()
-    s.install_django_project()
-    s.configure_supervisor()
+    try:
+        s.install_virtualenv()
+        s.start_virtualenv()
+        s.install_django_project()
+        s.configure_supervisor()
+    except:
+        #s.teardown()
+        pass
